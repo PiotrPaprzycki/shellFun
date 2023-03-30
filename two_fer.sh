67a56ea26d9f72d78e0e54bf910b2ab55f4d2407
PiotrPaprzycki <piotr.paprzycki@gmail.com>
2023-03-30 18:58:22 +0200

    Two fear problem, Shell Parameter Expansion

--- two_fer.sh
+++ two_fer.sh
@@ -13,9 +13,10 @@ If your friend doesn't like cookies, you give the cookie to the next person in l
 One for you, one for me.
 INTRO
 
-if [ -z $1 ]; then
- echo "One for you, one for me."
-else
- echo "One for $1, one for me."
-fi  
+#if [ -z $1 ]; then
+# echo "One for you, one for me."
+#else
+# echo "One for $1, one for me."
+#fi  
 
+echo "One for ${1:-you}, one for me."
